fad3f797740bb1593f719b000a15343f9f0049a1
Cuong Nguyen Cao <alobo.tech03@gmail.com>
2026-03-09 20:37:47 +0700

    docs(data-v2): update route field + data_v2

--- src/modules/routes/enums/route.enum.ts
+++ src/modules/routes/enums/route.enum.ts
@@ -7,4 +7,6 @@ export enum RouteStatus {
 export enum TransportType {
     PHO_THONG_CO_TRO_GIA = 'Phổ thông - Có trợ giá',
     PHO_THONG_KHONG_TRO_GIA = 'Phổ thông - Không trợ giá',
+    KHONG_TRO_GIA_DU_LICH = 'Không trợ giá - Du lịch',
+    HOC_SINH_CO_TRO_GIA = 'Học sinh - Có trợ giá',
 }
--- src/modules/routes/repositories/entities/route.entity.ts
+++ src/modules/routes/repositories/entities/route.entity.ts
@@ -5,11 +5,34 @@ import { TransportType, RouteStatus } from '@modules/routes/enums/route.enum';
 
 @Schema({ collection: 'routes_V3', timestamps: true })
 export class RouteEntity extends DBEntityBase {
+    /* Route */
+    @Prop({ required: false, type: String })
+    routeKey?: string;
+
     @Prop({ required: true, unique: true })
     routeCode: string;
 
     @Prop({ required: true, type: String })
     routeName: string;
+
+    @Prop({ required: false, type: String })
+    routeVarShortName?: string;
+
+    @Prop({ required: false })
+    startPoint?: string;
+
+    @Prop({ required: false })
+    endPoint?: string;
+
+    @Prop({ required: false, type: Boolean })
+    isOutbound?: boolean;
+
+    @Prop({ required: false, type: String })
+    runningTime?: string;
+
+    /* Route Detail */
+    @Prop({ required: false })
+    operatorName?: string;
 
     @Prop({
         required: true,
@@ -18,37 +41,29 @@ export class RouteEntity extends DBEntityBase {
     })
     transportType: TransportType;
 
-    @Prop({ required: false })
-    operatorName?: string;
-
-    // Ví dụ từ dataset: "ĐT: 028.3776.3777"
-    @Prop({ required: false, type: String })
-    phoneNumber?: string;
-
-    // Ví dụ từ dataset: "50 chỗ", "60 chỗ", "47 - 50 chỗ"
-    @Prop({ required: false, type: String })
-    vehicleType?: string;
-
-    @Prop({ required: false })
-    startPoint?: string;
-
-    @Prop({ required: false })
-    endPoint?: string;
-
-    // Ví dụ từ dataset: "15 - 18 phút"
-    @Prop({ required: false, type: String })
-    frequency: string;
-
-    // Ví dụ từ dataset: ["Vé lượt trợ giá: 5,000 VNĐ", "Vé lượt trợ giá HSSV: 3,000 VNĐ", ...]
-    @Prop({ required: false, type: [String] })
-    baseFare: string[];
-
-    // Ví dụ từ dataset: 8.59 (km)
     @Prop({ required: false, type: Number })
     totalDistance: number;
 
-    @Prop({ required: false, type: Boolean, default: false })
-    isWheelchairAccessible: boolean;
+    @Prop({ required: false, type: String })
+    vehicleType?: string;
+
+    @Prop({ required: false, type: String })
+    operatingTime?: string;
+
+    @Prop({ required: false, type: String })
+    phoneNumber?: string;
+
+    @Prop({ required: false, type: [String] })
+    baseFare: string[];
+
+    @Prop({ required: false, type: String })
+    numTrips?: string;
+
+    @Prop({ required: false, type: String })
+    tripTime?: string;
+
+    @Prop({ required: false, type: String })
+    frequency: string;
 
     @Prop({
         required: true,
@@ -57,43 +72,6 @@ export class RouteEntity extends DBEntityBase {
         default: RouteStatus.ACTIVE,
     })
     status: RouteStatus;
-
-    @Prop({
-        type: {
-            from: { type: String, required: false },
-            to: { type: String, required: false },
-        },
-        required: false,
-        _id: false,
-    })
-    operatingTime?: {
-        from: string;
-        to: string;
-    };
-
-    // Ví dụ từ dataset: "35 phút", "60 - 65 phút"
-    @Prop({ required: false, type: String })
-    tripTime?: string;
-
-    // Ví dụ từ dataset: "120 chuyến/ngày", "260 chuyến/ngày"
-    @Prop({ required: false, type: String })
-    numTrips?: string;
-
-    // Map<stationCode, distanceFromPrevious>
-    @Prop({
-        type: Map,
-        of: String,
-        default: {},
-    })
-    routeForwardCodes?: Map<string, string>;
-
-    // Map<stationCode, distanceFromPrevious>
-    @Prop({
-        type: Map,
-        of: String,
-        default: {},
-    })
-    routeBackwardCodes?: Map<string, string>;
 }
 
 export type RouteDoc = IDatabaseDocument<RouteEntity>;
