e3774a48d258d9fd8f1ef5e046e7caf441e114a0
Cuong Nguyen Cao <alobo.tech03@gmail.com>
2026-03-10 09:53:18 +0700

    refactor(service): add base service

--- src/modules/stations/services/station.service.ts
+++ src/modules/stations/services/station.service.ts
@@ -1,6 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { plainToInstance } from 'class-transformer';
-import { Document } from 'mongoose';
+import { Injectable } from '@nestjs/common';
 import { StationRepository } from '@modules/stations/repositories/repository/station.repository';
 import {
     StationEntity,
@@ -9,88 +7,23 @@ import {
 import { StationCreateRequestDto } from '@modules/stations/dtos/request/station-create.request.dto';
 import { StationUpdateRequestDto } from '@modules/stations/dtos/request/station-update.request.dto';
 import { StationGetResponseDto } from '@modules/stations/dtos/response/station-get.response.dto';
+import { BaseService } from '@common/services/base.service';
 
 @Injectable()
-export class StationService {
-    constructor(private readonly stationRepository: StationRepository) {}
-
-    mapGet(station: StationDoc | StationEntity): StationGetResponseDto {
-        const raw =
-            station instanceof Document ? station.toObject() : { ...station };
-
-        return plainToInstance(StationGetResponseDto, raw, {
-            excludeExtraneousValues: true,
-        });
-    }
-
-    mapList(stations: StationDoc[] | StationEntity[]): StationGetResponseDto[] {
-        return plainToInstance(
+export class StationService extends BaseService<
+    StationEntity,
+    StationDoc,
+    StationGetResponseDto,
+    StationCreateRequestDto,
+    StationUpdateRequestDto,
+    StationRepository
+> {
+    constructor(private readonly stationRepository: StationRepository) {
+        super(
+            stationRepository,
+            StationEntity,
             StationGetResponseDto,
-            stations.map((e: StationDoc | StationEntity) =>
-                e instanceof Document ? e.toObject() : { ...e },
-            ),
-            { excludeExtraneousValues: true },
+            'Station',
         );
-    }
-
-    async findAll(
-        filter: Record<string, any> = {},
-        page: number = 1,
-        limit: number = 10,
-    ): Promise<{ data: StationEntity[]; total: number }> {
-        const { data, total } =
-            await this.stationRepository.findAll<StationEntity>(
-                filter,
-                page,
-                limit,
-                { lean: true },
-            );
-
-        return { data, total };
-    }
-
-    async findOne(id: string): Promise<StationEntity> {
-        const station = await this.stationRepository.findOneById<StationEntity>(
-            id,
-            { lean: true },
-        );
-
-        if (!station) {
-            throw new NotFoundException(`Station with ID ${id} not found`);
-        }
-
-        return station;
-    }
-
-    async create(createDto: StationCreateRequestDto): Promise<StationDoc> {
-        const stationData = Object.assign(new StationEntity(), createDto);
-
-        return await this.stationRepository.create(stationData);
-    }
-
-    async update(
-        id: string,
-        updateDto: StationUpdateRequestDto,
-    ): Promise<StationDoc> {
-        const updateData = Object.assign(new StationEntity(), updateDto);
-
-        const updatedStation = await this.stationRepository.update<StationDoc>(
-            { _id: id },
-            updateData,
-        );
-
-        if (!updatedStation) {
-            throw new NotFoundException(`Station with ID ${id} not found`);
-        }
-
-        return updatedStation;
-    }
-
-    async delete(id: string): Promise<void> {
-        const deleted = await this.stationRepository.delete({ _id: id });
-
-        if (!deleted) {
-            throw new NotFoundException(`Station with ID ${id} not found`);
-        }
     }
 }
